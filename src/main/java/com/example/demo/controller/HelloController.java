8d3b20718ffe73ca199ab4abf545caac07bb6a92
andy <andy@andys-MacBook-Air.local>
2024-01-22 21:49:40 -0600

    add spring boot project to demo github action

---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello/{name}")
+    public String sayHello(@PathVariable("name") String name) {
+        return String.format("Say hello to %s from Spring Boot", name);
+    }
+
+}
